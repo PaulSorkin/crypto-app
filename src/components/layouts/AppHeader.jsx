80ea3f177f030dbdd2a43ef7f529fee4e656e0c6
ÑPaul Sorkin <sorkinpavel@mail.ru>
2024-01-31 17:34:04 -0600

    feat: add select and button from antd

--- src/components/layouts/AppHeader.jsx
+++ src/components/layouts/AppHeader.jsx
@@ -1,17 +1,60 @@
-import {Layout} from "antd";
+import {Button, Layout, Select, Space} from "antd";
 
 const headerStyle = {
+    width: '100%',
     textAlign: 'center',
-    color: '#fff',
     height: 60,
-    paddingInline: 48,
-    lineHeight: '64px',
-    backgroundColor: '#4096ff',
+    padding: '1rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
 };
+
+const options = [
+    {
+        label: 'China',
+        value: 'china',
+        emoji: '🇨🇳',
+        desc: 'China (中国)',
+    },
+    {
+        label: 'USA',
+        value: 'usa',
+        emoji: '🇺🇸',
+        desc: 'USA (美国)',
+    },
+    {
+        label: 'Japan',
+        value: 'japan',
+        emoji: '🇯🇵',
+        desc: 'Japan (日本)',
+    },
+    {
+        label: 'Korea',
+        value: 'korea',
+        emoji: '🇰🇷',
+        desc: 'Korea (韩国)',
+    },
+];
 
 
 export default function AppHeader() {
     return (
-        <Layout.Header style={headerStyle}>Header</Layout.Header>
+        <Layout.Header style={headerStyle}>
+            <Select
+                style={{
+                    width: 250,
+                }}
+                value="press / to open"
+                optionLabelProp="label"
+                options={options}
+                optionRender={(option) => (
+                    <Space>
+                        <img/> Coin Name
+                    </Space>
+                )}
+            />
+            <Button type="primary">Add Asset</Button>
+        </Layout.Header>
     )
 }
